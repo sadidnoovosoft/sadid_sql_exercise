d7e9b7d32b12a838ca31da7ac7a3a9ef8bf8197d
sadidnoovosoft <sadid.shaikh@noovosoft.com>
2023-08-29 20:45:30 +0530

    format sql files

--- sadid_01.sql
+++ sadid_01.sql
@@ -1,12 +1,12 @@
 with actor_lang as (
     select distinct a.first_name || ' ' || a.last_name as name,
                     l.name as language
-from film as f
-    join language as l using (language_id)
-    join film_actor as fa using (film_id)
-    join actor as a using (actor_id)
-group by (a.first_name || ' ' || a.last_name), l.language_id
-    )
+    from film as f
+        join language as l using (language_id)
+        join film_actor as fa using (film_id)
+        join actor as a using (actor_id)
+    group by (a.first_name || ' ' || a.last_name), l.language_id
+)
 select a1.name, a1.language from actor_lang as a1
 where (select count(1) from actor_lang as a2
        where a1.name = a2.name) > 1;
